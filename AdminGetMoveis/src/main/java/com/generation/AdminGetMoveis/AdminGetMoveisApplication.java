f7433748169faf7732951007e3ef49784a5667f1
Gabriel Fernando <oficial.gabriel89@gmail.com>
2020-06-23 00:34:31 -0300

    Sistema admin do Projeto Integrado GetMóveis

---
+++ AdminGetMoveis/src/main/java/com/generation/AdminGetMoveis/AdminGetMoveisApplication.java
@@ -0,0 +1,13 @@
+package com.generation.AdminGetMoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminGetMoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminGetMoveisApplication.class, args);
+	}
+
+}
